629cd62d376fbb623b322497a84bcf821d2676b7
temple7447 <Starukido@gmail.com>
2022-10-13 22:37:22 +0100

    hlo

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const btn = document.querySelector('.toggle-btn')
 const menu = document.getElementById('menu')
 
+
 btn.addEventListener('click', () => {
     btn.classList.toggle('active')
-        // menu.classList.toggle('flex')
-        // menu.classList.toggle('hidden')
-        // menu.classList.toggle('hidden')
+    menu.classList.toggle("active")
+
 })
+
+const counter = document.querySelector(".counter")
+const counter1 = document.querySelector(".counter1")
+
+
+let counters = 0;
+let couters1 = 0;
+
+while (counters < 10) {
+    let value = counters++;
+    counter.classList.toggle("addmore")
+    counter.innerHTML = value;
+}
